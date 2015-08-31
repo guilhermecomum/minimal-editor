d81bccd603ba9a9773518c4c8686d0920740a4fc
Guilherme Guerra <guerrinha@comum.org>
2015-08-31 15:39:35 -0300

    Protecting scope

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,53 @@
-function setStyle(style, param) {
-  var range, sel;
+(function(document){
 
-  if (window.getSelection) {
-    sel = window.getSelection();
-    if (sel.getRangeAt) {
-      range = sel.getRangeAt(0);
+  function setStyle(style, param) {
+    var range, sel;
+
+    if (window.getSelection) {
+      sel = window.getSelection();
+      if (sel.getRangeAt) {
+        range = sel.getRangeAt(0);
+      }
+      document.designMode = "on";
+      if (range) {
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }
+      document.execCommand(style, false, param);
+      document.designMode = "off";
     }
-    document.designMode = "on";
-    if (range) {
-      sel.removeAllRanges();
-      sel.addRange(range);
-    }
-    document.execCommand(style, false, param);
-    document.designMode = "off";
   }
-}
 
-Mousetrap.bind('mod+b', function(e) {
-  e.preventDefault();
-  setStyle("bold");
-});
+  Mousetrap.bind('mod+b', function(e) {
+    e.preventDefault();
+    setStyle("bold");
+  });
 
-Mousetrap.bind('mod+i', function(e) {
-  e.preventDefault();
-  setStyle("italic");
-});
+  Mousetrap.bind('mod+i', function(e) {
+    e.preventDefault();
+    setStyle("italic");
+  });
 
-Mousetrap.bind('mod+u', function(e) {
-  e.preventDefault();
-  setStyle("underline");
-});
+  Mousetrap.bind('mod+u', function(e) {
+    e.preventDefault();
+    setStyle("underline");
+  });
 
-function addImage(e) {
-  e.stopPropagation();
-  e.preventDefault();
+  function addImage(e) {
+    e.stopPropagation();
+    e.preventDefault();
 
-  var file = e.dataTransfer.files[0],
-      reader = new FileReader();
+    var file = e.dataTransfer.files[0],
+        reader = new FileReader();
 
-  reader.onload = function (event) {
-    setStyle("insertImage", event.target.result);
-  };
+    reader.onload = function (event) {
+      setStyle("insertImage", event.target.result);
+    };
 
-  reader.readAsDataURL(file);
-}
+    reader.readAsDataURL(file);
+  }
 
-var pad = document.getElementById('pad');
-pad.addEventListener('drop', addImage, false);
+  var pad = document.getElementById('pad');
+  pad.addEventListener('drop', addImage, false);
+
+})(document);
